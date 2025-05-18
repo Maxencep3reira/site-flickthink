744f21f1126a921c606f40b4a14697cf5b7dfab8
Maxencep3reira <maxencepereira8@hotmail.fr>
2025-05-18 17:26:13 +0200

    aa

---
+++ menu.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const burger = document.getElementById("burger");
+  const nav = document.getElementById("nav-links");
+
+  burger.addEventListener("click", () => {
+    nav.classList.toggle("active");
+    burger.classList.toggle("open");
+  });
+});
